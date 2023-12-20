da7032ed54b9abaf39917653347108bd70a3ae8e
wanwanQWQ <141887111+wanwanQWQ@users.noreply.github.com>
2023-12-21 05:48:12 +0800

    clonejdk.sh

--- clonejdk.sh
+++ clonejdk.sh
@@ -2,10 +2,10 @@
 set -e
 if [ "$TARGET_JDK" == "arm" ]; then
 git clone --depth 1 https://github.com/openjdk/aarch32-port-jdk8u openjdk
-elif [ "$BUILD_IOS" == "1" ]; then
+elif [ "$BUILD_IOS" == "0" ]; then
 git clone --depth 1 --branch ios https://github.com/PojavLauncherTeam/openjdk-multiarch-jdk8u openjdk
 else
 # Use aarch32 repo because it also has aarch64
 
-git clone --depth 1 https://github.com/openjdk/jdk8u openjdk
+git clone --depth 1 https://github.com/dragonwell-project/dragonwell8 openjdk
 fi
